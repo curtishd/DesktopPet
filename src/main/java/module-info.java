7f0e8a5a482c0d87e70f1dd8423d90d1f0747320
curtishd <1023632660@qq.com>
2024-09-29 17:19:05 +0800

    Add module file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module module.name {
+    exports me.cdh.Control;
+    exports me.cdh;
+    exports me.cdh.DrawControl;
+    exports me.cdh.DestopPet;
+    exports me.cdh.Draw;
+
+    requires transitive java.desktop;
+}
